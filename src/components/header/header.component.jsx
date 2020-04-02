bbfbf81adcfd3e4e0a449b529a1b9d182b5ed26b
Раида Мамедова <raidamamedova96@gmail.com>
2020-04-02 17:32:16 +0400

    initial commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./header.styles.scss"
+import Settings from "../settings/settings.component";
+
+const Header = () => (
+    <header>
+        <h2>Raida Mammad</h2>
+        <nav className="menu">
+            <a href="#projects">Projects</a>
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <Settings />
+    </header>
+);
+
+export default Header;
---
+++ src/components/settings/settings.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./settings.styles.scss";
+
+const Settings = () => (
+    <div className="header_settings">
+        <div className="mode">White</div>
+        <div className="language">EN</div>
+        <div className="cv_info">CV</div>
+    </div>
+);
+
+export default Settings;
