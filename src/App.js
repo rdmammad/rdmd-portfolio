4dfd4b7e069a759d48149f26de0c96eb1ad9895e
Раида Мамедова <raidamamedova96@gmail.com>
2020-04-03 18:55:31 +0400

    background image added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Projects from "./sections/projects/projects.component";
 import Footer from "./components/footer/footer.component";
 import Experience from "./sections/experience/experience.component";
 import Contact from "./sections/contact/contact.component";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="rdmammad__portfolio">
-        <Header/>
-        <Projects/>
+        <div className="background__header">
+            <Header/>
+            <Projects/>
+        </div>
         {/*<section id="about"></section>*/}
         <Experience/>
         <Contact/>
