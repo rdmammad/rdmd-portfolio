f131cd707d8a492cfcc368b391401aab39f893c3
Раида Мамедова <raidamamedova96@gmail.com>
2020-04-04 14:13:48 +0400

    socials added to contact, some section styling ches

--- src/sections/contact/contact.component.jsx
+++ src/sections/contact/contact.component.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import "./contact.styles.scss";
 import Informational from "../../components/informational/informational.component";
+import CustomIcon from "../../components/custom-icon/custom-icon.component";
+import {SOCIALS_DATA} from "./socials.data";
 
-const Contact = () => (
-    <section id="contact">
-        <Informational title="Contact">
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
 
-        </Informational>
-    </section>
-);
+        this.state = {
+            socials: SOCIALS_DATA,
+        };
+    }
+
+    render() {
+        const {socials} = this.state;
+
+        return (
+            <section id="contact">
+                <Informational title="Contact">
+                    <Informational title="Contact Me If You Have Any Question" sub_class="sub-block">
+                        <div className="rdmammad__sicials">
+                            {
+                                socials.map(({id, title, image_path, url}) =>
+                                    <CustomIcon key={id} title={title} image_path={image_path} href={url}
+                                                target="_blank"/>
+                                )
+                            }
+                        </div>
+                    </Informational>
+                </Informational>
+            </section>
+        );
+    }
+}
 
 export default Contact;
