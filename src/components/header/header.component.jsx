1f78899fbd2c66b692e47aefe8622cfd609679a5
Раида Мамедова <raidamamedova96@gmail.com>
2020-04-04 12:27:04 +0400

    header color on scroll ches

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,17 +2,38 @@ import React from "react";
 import "./header.styles.scss"
 import Settings from "../settings/settings.component";
 
-const Header = () => (
-    <header id="projects">
-        <h2>Raida Mammad</h2>
-        <nav className="menu">
-            <a href="#projects">Projects</a>
-            {/*<a href="#about">About</a>*/}
-            <a href="#experience">Experience</a>
-            <a href="#contact">Contact</a>
-        </nav>
-        <Settings />
-    </header>
-);
+class Header extends React.Component{
+    constructor(){
+        super();
 
+        this.state = {
+            header_class: '',
+        };
+    }
+
+    listenScrollEvent = () => {
+        this.setState({header_class: window.scrollY > 100 ? 'active' : ''});
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent);
+    }
+
+    render(){
+        const {header_class} = this.state;
+
+        return(
+            <header id='project' className={header_class}>
+                <h2>Raida Mammad</h2>
+                <nav className="menu">
+                    <a href="#projects">Projects</a>
+                    {/*<a href="#about">About</a>*/}
+                    <a href="#experience">Experience</a>
+                    <a href="#contact">Contact</a>
+                </nav>
+                <Settings />
+            </header>
+        )
+    }
+}
 export default Header;
